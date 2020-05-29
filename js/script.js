653bb1301a1c8ff497d167a8629c277994563b2b
Diogo Caldas <diogovdcpa@gmail.com>
2020-05-28 22:36:20 -0300

    update

--- js/script.js
+++ js/script.js
@@ -1,18 +1,102 @@
-window.addEventListener('load',changeColor)
+// declarando variaveis 
 
-function changeColor() {
-    var red = document.querySelector('#red').value;
-    var green = document.querySelector('#green').value;
-    var blue = document.querySelector('#blue').value;
-  
-    var color = 'Rgb(' + red + ',' + green + ',' + blue + ')';
-  
-    document.querySelector('#squareResult').style.background = color;
-    document.querySelector('#offRed').value = red;
-    document.querySelector('#offGreen').value = green;
-    document.querySelector('#offBlue').value = blue;
-  }
-  
-  document.querySelector('#red').addEventListener('input', changeColor);
-  document.querySelector('#green').addEventListener('input', changeColor);
-  document.querySelector('#blue').addEventListener('input', changeColor);
+let inputRangeRed = 0;
+let inputRangeGreen = 0;
+let inputRangeBlue = 0;
+
+let inputTextRed = 0;
+let inputTextGreen = 0;
+let inputTextBlue = 0;
+
+let divSquare = 0;
+
+let r = '0';
+let g = '0';
+let b = '0';
+
+// iniciando o carregamento do console
+window.addEventListener('load', () => {
+    mapElements()
+});
+
+
+// funçao para mapar o html no javascript
+function mapElements() {
+    inputRangeRed = document.querySelector('#inputRangeRed');
+    inputRangeGreen = document.querySelector('#inputRangeGreen');
+    inputRangeBlue = document.querySelector('#inputRangeBlue');
+
+    inputTextRed = document.querySelector('#inputTextRed');
+    inputTextGreen = document.querySelector('#inputTextGreen');
+    inputTextBlue = document.querySelector('#inputTextBlue');
+
+    divSquare = document.querySelector('#divSquare')
+
+    // escutando o evento change na função handleInputRangeChange
+    inputRangeRed.addEventListener('change', handleInputRangeChange)
+    inputRangeGreen.addEventListener('change', handleInputRangeChange)
+    inputRangeBlue.addEventListener('change', handleInputRangeChange)
+
+    inputTextRed.addEventListener('change',handleInputTextChange)
+    inputTextGreen.addEventListener('change',handleInputTextChange)
+    inputTextBlue.addEventListener('change',handleInputTextChange)
+
+};
+
+// funçao handleInputRangeChange que escuta o input range e retorna o numero
+function handleInputRangeChange(event) {
+    const value = event.target.value;
+    const id = event.target.id;
+
+    switch (id) {
+        case 'inputRangeRed':
+            r = value;
+            break;
+
+        case 'inputRangeGreen':
+            g = value;
+            break;
+        case 'inputRangeBlue':
+            b = value;
+            break;
+    }
+
+    syncDiv()
+}
+
+// funçao que desenha os valores rgb na div square
+function syncDiv() {
+    divSquare.style.backgroundColor = `rgb(${r} ${g} ${b})`;
+
+    inputTextRed.value = r;
+    inputTextGreen.value = g;
+    inputTextBlue.value = b;
+}
+
+function syncDivText() {
+    divSquare.style.backgroundColor = `rgb(${r} ${g} ${b})`;
+
+    inputRangeRed.value = r;
+    inputRangeGreen.value = g;
+    inputRangeBlue.value = b;
+}
+
+function handleInputTextChange() {
+    const value = event.target.valueAsNumber;
+    const id = event.target.id;
+
+    switch (id) {
+        case 'inputTextRed':
+            r = value;
+            break;
+
+        case 'inputTextGreen':
+            g = value;
+            break;
+        case 'inputTextBlue':
+            b = value;
+            break;
+    }
+
+    syncDivText()
+}
